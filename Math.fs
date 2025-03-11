19cef1806605174c851e8acad92d4f9c9fc18bfb
neutrino1359 <samsung@DESKTOP-15KH042>
2025-03-11 09:44:38 +0900

    quiz2_1

--- Math.fs
+++ Math.fs
@@ -6,4 +6,15 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  
+  let rec f (n:int64) t=
+    if n = 1 then t
+    else if n % 2L = 0 then f (n / 2L) (t + 1)
+    else f (3L * n + 1L) (t + 1)
+
+  f n 0
+
+
+
+
+
